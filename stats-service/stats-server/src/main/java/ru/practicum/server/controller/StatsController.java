a598de9249155b0accff59fb019662f07e18876d
Daniil Kuznetsov <daniilbacardi@yandex.ru>
2023-09-07 14:25:27 +0300

    stat service: initial commit

---
+++ stats-service/stats-server/src/main/java/ru/practicum/server/controller/StatsController.java
@@ -0,0 +1,41 @@
+package ru.practicum.server.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.dto.EndpointHit;
+import ru.practicum.dto.ViewStats;
+import ru.practicum.server.service.StatsService;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class StatsController {
+    private final StatsService statsService;
+
+    @PostMapping("/hit")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addHit(@RequestBody @Valid EndpointHit endpointHit) {
+        statsService.addHit(endpointHit);
+        log.info("StatsController: addHit выполнено {}", endpointHit);
+    }
+
+    @GetMapping("/stats")
+    public List<ViewStats> getStats(@RequestParam(value = "start")
+                                        @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+                                    @RequestParam (value = "end")
+                                    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+                                    @RequestParam (value = "uris", required = false) List<String> uris,
+                                    @RequestParam (value = "unique",
+                                            required = false, defaultValue = "false") Boolean unique) {
+        log.info("StatsController: getStats выполнено start = {}, end = {}, uris = {}, unique = {}",
+                start, end, uris, unique);
+        return statsService.getStats(start, end, uris, unique);
+    }
+}
---
+++ stats-service/stats-server/src/main/java/ru/practicum/server/service/StatsServiceImpl.java
@@ -0,0 +1,55 @@
+package ru.practicum.server.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.dto.EndpointHit;
+import ru.practicum.dto.ViewStats;
+import ru.practicum.server.exception.DateException;
+import ru.practicum.server.mapper.StatsMapper;
+import ru.practicum.server.repository.StatsRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional(readOnly = true)
+public class StatsServiceImpl implements StatsService {
+    private final StatsRepository statsRepository;
+
+    @Override
+    @Transactional
+    public void addHit(EndpointHit endpointHit) {
+        statsRepository.save(StatsMapper.dtoToModel(endpointHit));
+        log.info("StatsServiceImpl: addHit выполнено {}", endpointHit);
+    }
+
+    @Override
+    public List<ViewStats> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
+        validateDate(start, end);
+        log.info("StatsServiceImpl: getStats выполнено start = {}, end = {}, uris = {}, unique = {}",
+                start, end, uris, unique);
+        if (uris == null || uris.isEmpty()) {
+            if (unique) {
+                return statsRepository.getStatsWithUniqueIp(start, end);
+            } else {
+                return statsRepository.getStats(start, end);
+            }
+        } else {
+            if (unique) {
+                return statsRepository.getStatsAndUrisWithUniqueIp(start, end, uris);
+            } else {
+                return statsRepository.getStatsAndUris(start, end, uris);
+            }
+        }
+    }
+
+    private void validateDate(LocalDateTime start, LocalDateTime end) {
+        if (start.isAfter(end)) {
+            throw new DateException("Указанное значение end раньше значения start");
+        }
+    }
+}
